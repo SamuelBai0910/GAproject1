6c727561ed63ab1ec40322767505ffd3e0a58b1d
SamuelBai <abaid1024@gmail.com>
2023-07-10 15:21:12 +1000

    update the minus HTML CSS code

---
+++ README.md
@@ -0,0 +1,8 @@
+# GAproject1
+  Hi everyone, welcome to my first project, it's a web game called MasterMind.
+# MasterMind history
+
+# How to play
+
+# Explaination of code
+
---
+++ js/main.js
@@ -0,0 +1,30 @@
+/*
+Pseudocode for the overall gameplay
+1.Create a object to store data of code pegs {yello:1, ...white:6}
+2.Create a object to store data of key pegs {black:1, wihte:0}
+3.Generate a random secret code consisting of colored pegs.
+4.Write a init function that render() when game end.
+5.Set the maximum guesses round number of 10.
+6.Repeat untill player win or runs out of guesses round.
+  6.1. Display the game board with the current guess history.
+  6.2. Validate the player's guess.
+  6.3. Evaluate the player's guess against the secret code and provide feedback.
+  6.4. Add the player's guess to the guess history.
+  6.5. Decrease the number of remaining guesses.
+7.Display a victory message if player wins.
+8.Display a defeat message and reveal the secret code if player runs out of guesses round.
+*/
+
+/*----- constants -----*/
+
+
+/*----- state variables -----*/
+
+
+/*----- cached elements  -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
